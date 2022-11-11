0427f26a6d5a07f9f9ec67d88096808e5689bd2d
Abdullah Bilgin <abilgin02@icloud.com>
2022-11-11 14:17:57 +0300

    Add Color swift file to keep color for entire project, add main color

--- Address Book/AppColors.swift
+++ Address Book/AppColors.swift
@@ -5,4 +5,9 @@
 //  Created by Abdullah Bilgin on 11/11/22.
 //
 
-import Foundation
+import SwiftUI
+
+// created a structure for colors and add main color
+struct AppColor {
+    static let mainColor = (Color(red: 254/255, green: 240/255, blue: 229/255))
+}
