693f45da69397477cd1f6a1a001bc985aec66424
Abdullah Bilgin <abilgin02@icloud.com>
2022-11-11 14:03:24 +0300

    Initial Commit

---
+++ Address Book/Address_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Address_BookApp.swift
+//  Address Book
+//
+//  Created by Abdullah Bilgin on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Address_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
